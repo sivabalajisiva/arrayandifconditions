b1fb95ac311619f79e40992065eb6e9c5ca1979f
sivabalajisiva <150930405+sivabalajisiva@users.noreply.github.com>
2025-01-12 15:25:20 +0530

    Update myfirst.js

--- myfirst.js
+++ myfirst.js
@@ -64,10 +64,26 @@ db.sales.aggregate([
 //     "avgSales": 460            // (2300 / 5)
 // }]
 ----------------------------------------------------
-
-
-
-
+5:
+// [{  _id: 1, student: "Maya",  homework: [ 10, 5, 10 ], quiz: [ 10, 8 ], extraCredit: 0 },
+//    {  _id: 2, student: "Ryan", homework: [ 5, 6, 5 ],  quiz: [ 8, 8 ],  extraCredit: 8 }]
+db.scores.aggregate([
+   {
+     $addFields: {
+       totalHomework: { $sum: "$homework" } ,
+       totalQuiz: { $sum: "$quiz" }
+     }
+   },
+   {
+     $addFields: { totalScore:
+       { $add: [ "$totalHomework", "$totalQuiz", "$extraCredit" ] } }
+   }
+])
+// [{ _id: 1,student: "Maya",  homework: [ 10, 5, 10 ], quiz: [ 10, 8 ], extraCredit: 0,
+//   totalHomework: 25, totalQuiz: 18, totalScore: 43 },
+//   {_id: 2, student: "Ryan", homework: [ 5, 6, 5 ],quiz: [ 8, 8 ],  extraCredit: 8,
+//   totalHomework: 16, totalQuiz: 16,  totalScore: 40}]
+-----------------------------------------------------
 
 
 
